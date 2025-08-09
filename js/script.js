dcf7c9ec26197dcd15fbf3180d31d3230c0dc3f4
Kashaf25119 <kashafakhlaq25@gmail.com>
2025-08-09 10:27:51 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var typed = new Typed(".typing",{
+    strings:["Wishing you a day filled with love, laughter, and all your favorite things. Here's to another amazing year ahead! 🎉"],
+    typeSpeed:50,
+    BackSpeed:60,
+})
